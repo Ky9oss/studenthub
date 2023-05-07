fcc6fae586b13e93be2079c583f9ac4b5428077f
CurrenTTT <128702647+CurrenTTT@users.noreply.github.com>
2023-05-07 22:45:05 +0800

    Add files via upload

--- src/main/java/model/Course.java
+++ src/main/java/model/Course.java
@@ -1,9 +1,28 @@
 package model;
 
 import java.util.*;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.URISyntaxException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.json.JSONArray;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import org.json.JSONTokener;
 /**
  * 
  */
@@ -106,19 +125,41 @@ public class Course {
      */
     public static String getCoursesByYearAndByTypeForwardSort(int year, String type) {
         // TODO implement here
-        
-        try {
-            JSONObject jsonObject = new JSONObject(json);
+        //以下是新内容
 
-            if (jsonObject.has(Integer.toString(year)&&jsonObject.has(type) {
-                String value = jsonObject.get(Integer.toString(year)).getAsString();
-                System.out.println("Found matching value for year " + year + ": " + value);
-                return "Found matching value for year " + year;
-              }
-          } catch (IOException e) {
-            e.printStackTrace();
+        try {
+          // 读取JSON文件并解析为JSONArray对象
+          JSONTokener tokener = new JSONTokener(new FileReader("course.json"));
+          JSONArray jsonArray = new JSONArray(tokener);
+
+          // 将JSONArray对象转换为Java List对象
+          List<JSONObject> list = new ArrayList<JSONObject>();
+          for (int i = 0; i < jsonArray.length(); i++) {
+              list.add(jsonArray.getJSONObject(i));
           }
-        //
+
+          // 使用Collections.sort()方法对Java List对象进行排序
+          Collections.sort(list, new Comparator<JSONObject>() {
+              @Override
+              public int compare(JSONObject o1, JSONObject o2) {
+                  // 根据对象的某个值进行排序，这里假设JSON对象中有一个名为"value"的属性
+                  String val1 = o1.getInt("year");
+                  String val2 = o2.getInt("year");
+                  return val1.compareTo(val2);
+              }
+          });
+
+          // 将Java List对象转换回JSONArray对象
+          jsonArray = new JSONArray();
+          for (JSONObject obj : list) {
+              jsonArray.put(obj);
+          }
+
+          // 将JSONArray对象转换为JSON字符串并输出
+          System.out.println(jsonArray.toString(4));
+      } catch (Exception e) {
+          e.printStackTrace();
+      }
         
     }
 
@@ -149,10 +190,13 @@ public class Course {
                     jsonObject.getString("grade"),
                     jsonObject.getString("credit"),
                     
-                    jsonObject.getString("type"),
+                    jsonObject.getString("type")
                     
                   );
-                    results.StringBuffer(activity);
+                  results.StringBuffer(activity);
+                } catch (IOException e) {
+                  e.printStackTrace();
+              }
               }
             }
         
@@ -173,7 +217,9 @@ public class Course {
                     results = jsonObject.getString("course")
                     
                   );
-                    
+                }  catch (IOException e) {
+                  e.printStackTrace();
+              }
               }
             }
         
