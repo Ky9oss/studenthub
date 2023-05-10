c6992e26787894f855790567177b63eaca6f76af
Taylorblue123 <1173476875@qq.com>
2023-05-10 09:37:59 +0800

    wwx_CV_Gen_V1.0

---
+++ src/main/java/model/CVGeneratorMain.java
@@ -0,0 +1,13 @@
+public class CVGeneratorMain {
+    public static void main(String[] args) {
+        CVGenerator cvGenerator = new CVGenerator();
+
+        String basic_information = "{\"major\":\"major_content\",\"school\":\"school_content\",\"image_path\":\"/home/kadrex/Study/codes/java/jiti_lab/studenthub/src/main/resources/image.png\",\"name\":\"name_content\",\"graduation_time\":\"graduation_time_content\",\"admission_time\":\"admission_time_content\",\"age\":0}";
+        String skills = "[{\"title\": \"title_content\", \"content\": \"content_content\", \"proficiency\": \"Advanced(for example)\", \"project\": \"java(for example)\"},{\"title2\": \"hahahaha22\", \"content\": \"conten22\", \"proficiency\": \"Advanced\", \"project\": \"java22\"}]";
+        String activities = "[{\"title\": \"title_content\", \"content\": \"content_detail\", \"time\": \"xxxx-xx-xx\", \"type\": \"type_detail\", \"location\": \"beijing\"},{\"title\": \"title_content\", \"content\": \"content_detail\", \"time\": \"xxxx-xx-xx\", \"type\": \"type_detail\", \"location\": \"beijing(for example)\"}]";
+        String roles = "[{\"title\": \"title_content\", \"content\": \"content_content\", \"time\": \"xxxx-xx-xx\"},{\"title\": \"title_detail\", \"content\": \"content_content\", \"time\": \"xxxx-xx-xx\"}]";
+        String achievements = "[{\"title\": \"title_content\", \"content\": \"content_content\", \"time\": \"xxxx-xx-xx\", \"team\": \"111(for example)\", \"responsibility\": \"beijing\"},{\"title\": \"title_content\", \"content\": \"content_content\", \"time\": \"xxxx-xx-xx\", \"team\": \"222(example\", \"responsibility\": \"beijing(example)\"}]";
+
+        cvGenerator.generateCV(basic_information, skills, activities, roles, achievements);
+    }
+}
