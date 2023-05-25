924ea92bdd96199914a10f854c6c8ed2fb714343
wangyunsang <2002wangzl@bupt.edu.cn>
2023-05-25 21:24:52 +0800

    添加GPA算法

--- src/main/java/view/GPAControler.java
+++ src/main/java/view/GPAControler.java
@@ -1,19 +1,17 @@
 package view;
-import controller.*;
-
 import java.io.IOException;
 import java.net.URISyntaxException;
 
+import controller.Controller;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
-
 import javafx.stage.Stage;
-import javafx.scene.Node;
 // import javafx.scene.layout.VBox;
 // import java.io.IOException;
  public class GPAControler {
@@ -118,19 +116,19 @@ import javafx.scene.Node;
     @FXML
     void sf1(ActionEvent event) throws IOException, URISyntaxException{
 
-          detail.setText("算法1");
+          detail.setText("The standard 4.0 GPA algorithm assigns a numerical value to each letter grade, ranging from 4.0 for an A to 0.0 for an F. The algorithm uses a fixed range of scores to determine the corresponding GPA value for each letter grade, as follows:"+"\n"+"A grade of 90-100 corresponds to a GPA of 4.0\nB grade of 80-89 corresponds to a GPA of 3.0\nC grade of 70-79 corresponds to a GPA of 2.0\nD grade of 60-69 corresponds to a GPA of 1.0\nF grade below 60 corresponds to a GPA of 0.0");
           gpa.setText(""+Controller.calculateGradePointAverage(1));
     }
 
     @FXML
     void sf2(ActionEvent event) throws IOException, URISyntaxException{
-        detail.setText("算法2");
+        detail.setText("The WES GPA algorithm assigns a numerical value to each letter grade, ranging from 4.0 for an A to 1.0 for a D. The algorithm uses a fixed range of scores to determine the corresponding GPA value for each letter grade, as follows:\nA grade of 85-100 corresponds to a GPA of 4.0\nB grade of 75-84 corresponds to a GPA of 3.0\nC grade of 60-74 corresponds to a GPA of 2.0\nD grade below 60 corresponds to a GPA of 1.0");
         gpa.setText(""+Controller.calculateGradePointAverage(2));
     }
 
     @FXML
     void sf3(ActionEvent event) throws IOException, URISyntaxException{
-        detail.setText("算法3");
+        detail.setText("The PKU 4.0 GPA algorithm assigns a numerical value to each letter grade, ranging from 4.0 for an A to 0.0 for an F. The algorithm uses a fixed range of scores to determine the corresponding GPA value for each letter grade, as follows:\nA grade of 90-100 corresponds to a GPA of 4.0\nA- grade of 85-89 corresponds to a GPA of 3.7\nB+ grade of 82-84 corresponds to a GPA of 3.3\nB grade of 78-81 corresponds to a GPA of 3.0\nB- grade of 75-77 corresponds to a GPA of 2.7\nC+ grade of 72-74 corresponds to a GPA of 2.3\nC grade of 68-71 corresponds to a GPA of 2.0\nC- grade of 65-67 corresponds to a GPA of 1.5\nD grade of 60-64 corresponds to a GPA of 1.0\nF grade below 60 corresponds to a GPA of 0.0.");
         gpa.setText(""+Controller.calculateGradePointAverage(3));
     }
  }
